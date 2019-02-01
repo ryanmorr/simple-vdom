c16f3f07a364fedbbfe254503748355dd0fb3333
Ryan Morr <rm.morr@gmail.com>
2019-02-01 13:16:08 -0500

    Build v0.1.1

--- dist/vdom.js
+++ dist/vdom.js
@@ -1,15 +1,15 @@
-/*! simple-vdom v0.1.0 | https://github.com/ryanmorr/simple-vdom */
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.simpleVdom = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-'use strict';
+/*! @ryanmorr/simple-vdom v0.1.1 | https://github.com/ryanmorr/simple-vdom */
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.vdom = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
-
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
 exports.patch = patch;
 exports.h = h;
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 /**
  * Add or remove an attribute/event to
  * and from an element
@@ -21,24 +21,26 @@ exports.h = h;
  * @api private
  */
 function updateAttribute(element, name, newVal) {
-    var oldVal = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : null;
+  var oldVal = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : null;
 
-    if (name[0] === 'o' && name[1] === 'n') {
-        name = name.slice(2).toLowerCase();
-        if (newVal == null) {
-            element.removeEventListener(name, oldVal);
-        } else if (oldVal == null) {
-            element.addEventListener(name, newVal);
-        }
-        return;
+  if (name[0] === 'o' && name[1] === 'n') {
+    name = name.slice(2).toLowerCase();
+
+    if (newVal == null) {
+      element.removeEventListener(name, oldVal);
+    } else if (oldVal == null) {
+      element.addEventListener(name, newVal);
     }
-    if (newVal == null || newVal === false) {
-        element.removeAttribute(name);
-    } else {
-        element.setAttribute(name, newVal);
-    }
+
+    return;
+  }
+
+  if (newVal == null || newVal === false) {
+    element.removeAttribute(name);
+  } else {
+    element.setAttribute(name, newVal);
+  }
 }
-
 /**
  * Create a DOM node from a virtual
  * node
@@ -50,21 +52,23 @@ function updateAttribute(element, name, newVal) {
  * @return {Element}
  * @api private
  */
-function createElement(vnode) {
-    if (typeof vnode === 'string') {
-        return document.createTextNode(vnode);
-    }
-    var element = document.createElement(vnode.nodeName),
-        attributes = vnode.attributes;
-    Object.keys(attributes).forEach(function (name) {
-        return updateAttribute(element, name, attributes[name]);
-    });
-    vnode.children.map(createElement).forEach(function (node) {
-        return element.appendChild(node);
-    });
-    return element;
-}
 
+
+function createElement(vnode) {
+  if (typeof vnode === 'string') {
+    return document.createTextNode(vnode);
+  }
+
+  var element = document.createElement(vnode.nodeName),
+      attributes = vnode.attributes;
+  Object.keys(attributes).forEach(function (name) {
+    return updateAttribute(element, name, attributes[name]);
+  });
+  vnode.children.map(createElement).forEach(function (node) {
+    return element.appendChild(node);
+  });
+  return element;
+}
 /**
  * Recursively patch a DOM element so that
  * it matches the updated virtual tree
@@ -75,29 +79,33 @@ function createElement(vnode) {
  * @param {Number} index (optional)
  * @api public
  */
+
+
 function patch(parent, newNode) {
-    var oldNode = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
-    var index = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 0;
+  var oldNode = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
+  var index = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 0;
+  var element = parent.childNodes[index];
 
-    var element = parent.childNodes[index];
-    if (oldNode == null) {
-        return parent.appendChild(createElement(newNode));
+  if (oldNode == null) {
+    return parent.appendChild(createElement(newNode));
+  }
+
+  if (newNode == null) {
+    return parent.removeChild(element);
+  } else if (_typeof(newNode) !== _typeof(oldNode) || typeof newNode === 'string' && newNode !== oldNode || newNode.nodeName !== oldNode.nodeName) {
+    return parent.replaceChild(createElement(newNode), element);
+  }
+
+  if (newNode.nodeName) {
+    for (var name in Object.assign({}, newNode.attributes, oldNode.attributes)) {
+      updateAttribute(element, name, newNode.attributes[name], oldNode.attributes[name]);
     }
-    if (newNode == null) {
-        return parent.removeChild(element);
-    } else if ((typeof newNode === 'undefined' ? 'undefined' : _typeof(newNode)) !== (typeof oldNode === 'undefined' ? 'undefined' : _typeof(oldNode)) || typeof newNode === 'string' && newNode !== oldNode || newNode.nodeName !== oldNode.nodeName) {
-        return parent.replaceChild(createElement(newNode), element);
+
+    for (var i = 0; i < Math.max(newNode.children.length, oldNode.children.length); ++i) {
+      patch(element, newNode.children[i], oldNode.children[i], i);
     }
-    if (newNode.nodeName) {
-        for (var name in Object.assign({}, newNode.attributes, oldNode.attributes)) {
-            updateAttribute(element, name, newNode.attributes[name], oldNode.attributes[name]);
-        }
-        for (var i = 0; i < Math.max(newNode.children.length, oldNode.children.length); ++i) {
-            patch(element, newNode.children[i], oldNode.children[i], i);
-        }
-    }
+  }
 }
-
 /**
  * JSX-compatible virtual DOM builder
  *
@@ -107,13 +115,19 @@ function patch(parent, newNode) {
  * @return {Object}
  * @api public
  */
-function h(nodeName, attributes) {
-    for (var _len = arguments.length, children = Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
-        children[_key - 2] = arguments[_key];
-    }
 
-    attributes = attributes || {};
-    return { nodeName: nodeName, attributes: attributes, children: children };
+
+function h(nodeName, attributes) {
+  for (var _len = arguments.length, children = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+    children[_key - 2] = arguments[_key];
+  }
+
+  attributes = attributes || {};
+  return {
+    nodeName: nodeName,
+    attributes: attributes,
+    children: children
+  };
 }
 
 },{}]},{},[1])(1)
